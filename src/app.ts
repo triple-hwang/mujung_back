e0bfc007ca62053b6c6e985bbabbafba3e842eae
hjy080530 <164326669+hjy080530@users.noreply.github.com>
2025-07-06 23:16:01 +0900

    feat :: oauth 라우팅 만들기

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import songRoutes from './routes/songs.route';
 import votesRoute from "./routes/votes.route";
 import searchRoute from "./routes/search.route";
+import oauthRouter from './routes/oauth';
 
 dotenv.config();
 
@@ -13,4 +14,6 @@ app.use(express.json());
 app.use('/votes', votesRoute);
 app.use('/songs', songRoutes);
 app.use('/search',searchRoute);
+app.use('/oauth', oauthRouter); 
+
 export default app;
